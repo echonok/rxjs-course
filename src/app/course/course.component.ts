0db785dd194b044b3e7125e7bbdc6ddd68003d26
Nick Elobsky <echonok@gmail.com>
2023-02-24 17:15:12 +0100

    added forkJoin

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,8 +1,8 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../model/course';
-import { debounceTime, distinctUntilChanged, map, startWith, switchMap, } from 'rxjs/operators';
-import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, startWith, switchMap, tap, } from 'rxjs/operators';
+import { forkJoin, fromEvent, Observable, pipe } from 'rxjs';
 import { Lesson } from '../model/lesson';
 import { createHttpObservable } from '../common/util';
 import { debug, RxJSLogging, setRxjsLoggingLevel } from '../common/debug';
@@ -27,10 +27,16 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.courseId = this.route.snapshot.params['id'];
-    this.course$ = createHttpObservable<Course>(`/api/courses/${this.courseId}`)
+    const course$ = createHttpObservable<Course>(`/api/courses/${this.courseId}`);
+    const lessons$ = this.loadLessons();
+    forkJoin([course$, lessons$])
       .pipe(
-        debug(RxJSLogging.INFO, 'course value'),
-      );
+        tap(([first, second]) => {
+          console.log({ first });
+          console.log({ second });
+        }),
+      )
+      .subscribe();
   }
 
   ngAfterViewInit() {
